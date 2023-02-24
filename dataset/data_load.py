669453b9db655dab760bbe2a1af44adc7307aa95
zya <yonganzhong@outlook.com>
2023-02-24 14:30:14 +0800

    update data load

--- dataset/data_load.py
+++ dataset/data_load.py
@@ -2,22 +2,30 @@ import torch
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
-__all__ = ['train_dataset', 'test_dataset']
+__all__ = ['get_train_dataset', 'get_test_dataset']
 
-train_dataset = datasets.CIFAR100('../data', train=True, download=False,
-                                  transform=transforms.Compose([
-                                           transforms.RandomHorizontalFlip(),
-                                           transforms.RandomCrop(32, padding=4),
-                                           transforms.ToTensor(),
-                                           transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
-                                       ]))
 
-test_dataset = datasets.CIFAR100('../data', train=False, download=False,
-                                 transform=transforms.Compose([
-                                           transforms.ToTensor(),
-                                           transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+def get_train_dataset(data_dir='../data', download=False):
+
+    train_dataset = datasets.CIFAR100(data_dir, train=True, download=download,
+                                      transform=transforms.Compose([
+                                               transforms.RandomHorizontalFlip(),
+                                               transforms.RandomCrop(32, padding=4),
+                                               transforms.ToTensor(),
+                                               transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
                                            ])
-                                 )
+                                      )
+    return train_dataset
+
+
+def get_test_dataset(data_dir='../data', download=False):
+    test_dataset = datasets.CIFAR100(data_dir, train=False, download=download,
+                                     transform=transforms.Compose([
+                                               transforms.ToTensor(),
+                                               transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+                                               ])
+                                     )
+    return test_dataset
 
 
 if __name__ == "__main__":
@@ -28,6 +36,8 @@ if __name__ == "__main__":
     std = [0.2023, 0.1994, 0.2010]
 
     batch_size = 1
+
+    train_dataset = get_train_dataset('../data')
     train_loader = torch.utils.data.DataLoader(train_dataset,
                                                batch_size=batch_size,
                                                num_workers=1,
