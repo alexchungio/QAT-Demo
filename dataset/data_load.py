df22b1fd59f7ea9f8e5dbe3a3e9cf9d73d11803a
alexchung <yonganzhong@outlook.com>
2023-03-28 13:50:58 +0800

    update load data

--- dataset/data_load.py
+++ dataset/data_load.py
@@ -35,23 +35,28 @@ if __name__ == "__main__":
     mean = [0.4914, 0.4822, 0.4465]
     std = [0.2023, 0.1994, 0.2010]
 
-    batch_size = 1
+    batch_size = 128
+    num_epoch = 10
 
+    distribute = False
     train_dataset = get_test_dataset('../data', True)
+    if distribute:
+        sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
+    else:
+        sampler = torch.utils.data.sampler.RandomSampler(train_dataset)
+
     train_loader = torch.utils.data.DataLoader(train_dataset,
                                                batch_size=batch_size,
-                                               num_workers=1,
-                                               shuffle=True)
-    for batch_data in train_loader:
+                                               sampler=sampler,
+                                               num_workers=1)
 
-        img_array = batch_data[0][0].numpy()
-        img_array = img_array * np.array(std, dtype=float).reshape((3, 1, 1)) + np.array(mean, dtype=float).reshape((3, 1, 1))
-        img_array = np.array(img_array * 255, dtype=np.uint8)
-        img = np.transpose(img_array, (1, 2, 0))
-        cv2.imshow('img', img)
-        cv2.waitKey()
-
-
-
-
-
+    for epoch in range(num_epoch):
+        if distribute:
+            train_loader.sampler.set_epoch(epoch)
+        for batch_data in train_loader:
+            img_array = batch_data[0][0].numpy()
+            img_array = img_array * np.array(std, dtype=float).reshape((3, 1, 1)) + np.array(mean, dtype=float).reshape((3, 1, 1))
+            img_array = np.array(img_array * 255, dtype=np.uint8)
+            img = np.transpose(img_array, (1, 2, 0))
+            cv2.imshow('img', img)
+            cv2.waitKey()
